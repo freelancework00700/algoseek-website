38e2317b836d2d9256543f60d3f5c5d732e343ed
freelancework00700 <freelancework00700@gmail.com>
2024-12-18 13:25:30 +0530

    Completed header and home stats component

---
+++ src/app/pages/home/components/home-stats/home-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-stats',
+  imports: [],
+  templateUrl: './home-stats.component.html',
+  styleUrl: './home-stats.component.scss'
+})
+export class HomeStatsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroSliderComponent } from './components/hero-slider/hero-slider.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HomeStatsComponent } from './components/home-stats/home-stats.component';
 
 @Component({
   selector: 'app-home',
   styleUrl: './home.component.scss',
   templateUrl: './home.component.html',
-  imports: [HeroSliderComponent],
+  imports: [HeroSliderComponent, HeaderComponent, HomeStatsComponent],
 })
 export class HomeComponent {}
