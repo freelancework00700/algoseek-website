d64a7632be95c31b8211d19366e2833c6696442b
freelancework00700 <freelancework00700@gmail.com>
2024-12-18 09:24:19 +0530

    added prettier

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  styleUrl: './home.component.scss',
+  templateUrl: './home.component.html',
+  imports: [],
+})
+export class HomeComponent {}
